64f493c6212e8d2c7ca7924b29c48862e4254642
gustavooth <gustavooth@gustavo-pc.168.100.18>
2026-02-12 15:57:19 -0300

    vkinstance

--- src/main.c
+++ src/main.c
@@ -1,7 +1,42 @@
 #include "stdio.h"
 #include <vulkan/vulkan.h>
+#include "defines.h"
 
+typedef struct VkContext {
+  VkInstance instance;
+} VkContext;
+
+VkContext ctx;
+
+b8 create_instance() {
+  printf("Creating instance ... ");
+
+  VkApplicationInfo app_info = {0};
+  app_info.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+  app_info.pApplicationName = "VULKAN-GUIDE";
+  app_info.applicationVersion = VK_MAKE_VERSION(0, 1, 1);
+  app_info.pEngineName = "VULKAN-GUIDE-ENGINE";
+  app_info.engineVersion = VK_MAKE_VERSION(0, 1, 1);
+  app_info.apiVersion = VK_API_VERSION_1_3;
+
+  VkInstanceCreateInfo instance_info = {0};
+  instance_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+
+  if(vkCreateInstance(&instance_info, NULL, &ctx.instance) != VK_SUCCESS) {
+    printf("FAIL\n");
+    return false;
+  }
+
+  printf("SUCCESS\n");
+  return true;
+}
+
+void vk_init() {
+  if(!create_instance()) {
+    return;
+  }
+}
 
 int main() {
-  printf("Hello wolrd!");
+  vk_init();
 }
