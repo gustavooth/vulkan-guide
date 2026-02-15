d23f6e4bcebd4eb033cb17761a2a83807a6513d8
gustavooth <gustavooth@gustavo-pc.168.100.18>
2026-02-15 20:47:00 -0300

    image views

--- src/main.c
+++ src/main.c
@@ -35,6 +35,7 @@ typedef struct VkContext {
   VkSwapchainKHR swapchain;
   u32 swapchain_image_count;
   VkImage *swapchain_images;
+  VkImageView *swapchain_image_views;
 } VkContext;
 
 VkContext ctx = {0};
@@ -289,6 +290,32 @@ b8 create_swapchain() {
     return false;
   }
 
+  printf("Creating image views ... ");
+  ctx.swapchain_image_views = malloc(sizeof(VkImageView) * ctx.swapchain_image_count);
+  for (u32 i = 0; i < ctx.swapchain_image_count; i++)
+  {
+    VkImageViewCreateInfo view_info = {0};
+    view_info.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+    view_info.image = ctx.swapchain_images[i];
+    view_info.viewType = VK_IMAGE_VIEW_TYPE_2D;
+    view_info.format = VK_FORMAT_B8G8R8A8_SRGB;
+    view_info.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+    view_info.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+    view_info.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+    view_info.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+    view_info.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+    view_info.subresourceRange.baseMipLevel = 0;
+    view_info.subresourceRange.levelCount = 1;
+    view_info.subresourceRange.baseArrayLayer = 0;
+    view_info.subresourceRange.layerCount = 1;
+
+    if(vkCreateImageView(ctx.device, &view_info, NULL, &ctx.swapchain_image_views[i]) != VK_SUCCESS) {
+      printf("vkCreateImageView FAIL %u", i);
+      return false;
+    }
+  }
+  
+
   printf("SUCCESS\n");
   return true;
 }
@@ -351,6 +378,8 @@ b8 vk_init() {
 
 void vk_cleanup() {
   vkDeviceWaitIdle(ctx.device);
+
+  vkDestroySwapchainKHR(ctx.device, ctx.swapchain, NULL);
   vkDestroyCommandPool(ctx.device, ctx.command_pool, NULL);
 
   vkDestroyDevice(ctx.device, NULL);
