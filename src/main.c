e74993a2141791ddf987893ea4040405807562c9
gustavooth <gustavooth@gustavo-pc.168.100.18>
2026-02-16 12:03:07 -0300

    render pass

--- src/main.c
+++ src/main.c
@@ -36,6 +36,8 @@ typedef struct VkContext {
   u32 swapchain_image_count;
   VkImage *swapchain_images;
   VkImageView *swapchain_image_views;
+
+  VkRenderPass render_pass;
 } VkContext;
 
 VkContext ctx = {0};
@@ -320,6 +322,54 @@ b8 create_swapchain() {
   return true;
 }
 
+b8 create_render_pass() {
+  printf("Creating Render Pass ... ");
+
+  VkAttachmentDescription color_attachment = {0};
+  color_attachment.format = VK_FORMAT_B8G8R8A8_SRGB;
+  color_attachment.samples = VK_SAMPLE_COUNT_1_BIT;
+  color_attachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+  color_attachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+  color_attachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+  color_attachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+  color_attachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+  color_attachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+  VkAttachmentReference color_attachment_ref = {0};
+  color_attachment_ref.attachment = 0;
+  color_attachment_ref.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+  VkSubpassDescription subpass = {0};
+  subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+  subpass.colorAttachmentCount = 1;
+  subpass.pColorAttachments = &color_attachment_ref;
+
+  VkSubpassDependency dependency = {0};
+  dependency.srcSubpass = VK_SUBPASS_EXTERNAL;
+  dependency.dstSubpass = 0;
+  dependency.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+  dependency.srcAccessMask = 0;
+  dependency.dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+  dependency.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+
+  VkRenderPassCreateInfo render_pass_info = {0};
+  render_pass_info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+  render_pass_info.attachmentCount = 1;
+  render_pass_info.pAttachments = &color_attachment;
+  render_pass_info.subpassCount = 1;
+  render_pass_info.pSubpasses = &subpass;
+  render_pass_info.dependencyCount = 1;
+  render_pass_info.pDependencies = &dependency;
+
+  if (vkCreateRenderPass(ctx.device, &render_pass_info, NULL, &ctx.render_pass) != VK_SUCCESS) {
+    printf("vkCreateRenderPass FAIL\n");
+    return false;
+  }
+
+  printf("SUCCESS\n");
+  return true;
+}
+
 b8 frame() {
   vkResetCommandBuffer(ctx.command_buffers[0], 0);
 
@@ -370,6 +420,9 @@ b8 vk_init() {
     return false;
   }
   if(!create_swapchain()) {
+    return false;
+  }
+  if(!create_render_pass()) {
     return false;
   }
 
